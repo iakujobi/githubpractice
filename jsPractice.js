d5d767d19cc656b2e28c88919c0e7c0a8910bbb8
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-06-28 22:23:14 -0400

    Objects in JS

---
+++ jsPractice.js
@@ -0,0 +1,19 @@
+function tryingObjects()
+{
+    var employees = [
+        {
+            'name': 'sam',
+            'age': 10,
+            'id': 2021,
+            'address': '123 Fake Street'
+        },
+        {
+            'name': 'john',
+            'age': 20
+        }
+    ]
+    console.log(employees[0].name)
+}
+
+
+tryingObjects()
